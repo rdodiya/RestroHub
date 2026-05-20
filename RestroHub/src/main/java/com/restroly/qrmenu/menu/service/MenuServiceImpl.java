630cca2a556e54feb16759647df3fc3f7b2f6ff5
iRahmanG <rahmanmr039@gmail.com>
2026-05-21 01:52:20 +0530

    fix: refine logging after PR review

--- RestroHub/src/main/java/com/restroly/qrmenu/menu/service/MenuServiceImpl.java
+++ RestroHub/src/main/java/com/restroly/qrmenu/menu/service/MenuServiceImpl.java
@@ -51,6 +51,7 @@ public class MenuServiceImpl implements MenuService {
             // Check duplicate name within same branch
             if (menuRepository.existsByMenuNameAndBranch_BranchId(
                     requestDTO.getMenuName(), requestDTO.getBranchId())) {
+                    log.warn("Attempt to create duplicate menu: {}", requestDTO.getMenuName());
                 throw new DuplicateResourceException(
                         "Menu with name '" + requestDTO.getMenuName() +
                                 "' already exists for this branch");
@@ -139,9 +140,8 @@ public class MenuServiceImpl implements MenuService {
             // Check duplicate name (excluding current menu)
             if (menuRepository.existsByMenuNameAndBranch_BranchIdAndMenuIdNot(
                     requestDTO.getMenuName(), requestDTO.getBranchId(), menuId)) {
-                throw new DuplicateResourceException(
-                        "Menu with name '" + requestDTO.getMenuName() +
-                                "' already exists for this branch");
+                        log.warn("Attempt to update menu to duplicate name: {}", requestDTO.getMenuName());
+                        throw new DuplicateResourceException("Menu with name '" + requestDTO.getMenuName()+"' already exists for this branch");
             }
         }
 
